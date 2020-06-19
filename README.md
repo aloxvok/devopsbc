9daeacf71bffb8357b32f8769ee1870c45ef3356
aloxvok <hpinot9@gmail.com>
2020-06-18 21:41:55 -0700

    Copied content of hello world from the other branch

--- README.md
+++
@@ -1,7 +0,0 @@
-# DevOps BC
-
-This project it's to learn DevOps
-
-1. [Hello world](hello-world.py)
-2."upside down orange trees with cinnamon trees revolving around themin its proper state"[Hello world](hello-world.py)
-'counting on you branch master'
--- hello-world.py
+++ hello-world.py
@@ -1,1 +1,202 @@
 print ("Hello DevOps World")
+print ("Welcome")
+# print ("Non Po sso Piu Divider Me Tra Te e Il Mare")
+# print ("ahh!!! che cosa sta facendo sei seduto come una femminuccia")
+# print ("queste e un cartone animato chi my piace molto, il canne fifonne")
+
+
+# print ("questa è la storia di un uomo che visse nell'anticca città")
+
+
+# print ("te vas a la mierda\n\n\n")
+# a = 4
+# b = 2
+# c = a + b
+# print (c)
+# print ("o----")
+# print (" ||||")
+# print ("*" * 10)
+# price = 10
+# price = 20
+# rating = 4.9
+# print (price)
+
+
+# name = input("what is your name? ")
+# favorite_color = input("what is your favorite color? ")
+# print(name + " likes " + favorite_color)
+# birth_year = input("birth year: ")
+# age = 2019 - int(birth_year)
+# print(type(age))
+# print(age)
+# weight_lbs = input("weight(lbs): ")
+# weight_kg = int(weight_lbs) * 0.45
+# print(weight_kg)
+# print ("te vas a la mierda\n\n\n")
+# course = 'Python for "beginners"'
+# print(course)
+# course = '''
+# Hi Ale,
+
+
+# Here is this email to show our support.
+
+# Thank you,
+# The support cube
+
+
+# '''
+# course = 'Python for Beginners'
+# print(course)
+
+# print(course[1:-1])
+# print(course[2:-1])
+# print(course[-0:-4])
+first = "Marco"
+last = "Polo"
+msg = f"{first} [{last}] is a coder"
+print(msg)
+course = 'Python for Beginners'
+print(len(course))
+print(course.upper())
+print(len(course))
+print(course)
+
+
+
+print ("Welcome to class!")
+msg = f"{first} [{last}] is a coder"
+print ("Welcome to class!")
+
+if True:
+   print("True")
+else:
+   print("False")
+
+word = 'word'
+sentence = "This is a sentence."
+paragraph = """This is a paragraph. It is
+made up of multiple lines and sentences."""
+
+
+counter = 100        # An integer assignment
+miles   = 1000.0     # A floating point
+name    = "John"     # A string
+
+print(counter)
+print(miles)
+print(name)
+
+PI = 3.14159
+
+result = True;
+cUpper= 'C';
+b = 100;
+s = 10000;
+i = 100000;
+print(result)
+print(cUpper)
+print(result)
+
+
+result = f"{True} [{'c'}] b = 100 s =10000 i = 100000 son los codigos"
+print(result)
+course = 'Python for Beginners'
+print(course. replace('Beginners' , 'absolute Beginners'))
+print(course. replace('P', 'J'))
+print('Python' in course)
+print('python' in course)
+print(10 * 5)
+print(10 + 20)
+print(10 // 5)
+print(10 % 3)
+print(10 ** 3) # exponets
+x = 10
+x = x + 3
+print(x)
+import math
+x = 10
+x -= 3
+print(x)
+
+x = 10 + 3 * 2
+print(x)
+x = 10 + 3 * 2 ** 2
+print(x)
+x = (2 + 3) * 10 - 3
+print(x)
+x = 2.9
+print(round(x))
+x = 2.9
+print(abs(-2.9))
+import math
+print(math.ceil(2.9))
+print(math.floor(2.9))
+is_hot = True
+
+if is_hot:
+    print("it's a hot day")
+    print("Drink plenty of water")
+else:
+    print("It's a cold day")
+    print("Wear warm clothes")
+print("enjoy your day")
+print("have a great day\n\n\n")
+
+is_hot = False
+is_cold = False
+
+if is_hot:
+    print("It's a hoy day")
+    print("Drink plenty of water")
+elif is_cold:
+    print("It's a cold day")
+    print("Wear warm clothes")
+else:
+    print("It's a lovely day")
+
+print("Enjoy your day")
+
+is_hot = False
+is_cold = False
+
+if is_hot:
+    print("It's a hot day")
+    print("Drink plenty of water")
+elif is_cold:
+       print("It's a cold day")
+       print("Wear warm clothes")
+
+else:
+    print("It's a lovely day")
+print("Enjoy your day")
+print("have a great day\n\n\n")
+
+
+
+price  = 1000000
+has_good_credit = True
+
+if has_good_credit:
+    down_payment = 0.1 * price
+else:
+    down_payment = 0.2 * price
+print(f"Down payment: {down_payment}")
+
+# logical operators
+# if an applicant has a high income  AND good credit
+
+# logical AND
+
+has_high_income = True
+has_good_credit = True
+
+if has_high_income and has_good_credit:
+        print("Eligible for loan")
+
+# logical OR
+'''
+
+sending to github
+
+'''
