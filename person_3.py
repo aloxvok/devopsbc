ba5a5d7a88ada055c86de4249b0de5194a32e741
Hector Pinot <hpinot9@gmail.com>
2020-07-22 18:53:06 -0700

    Update person_3.py

--- person_3.py
+++ person_3.py
@@ -10,7 +10,7 @@ class Person:
     def say_hello(self):
         print("Hello :) My name is", self.name, "I am", self.age, "years old, enrolled in", self.program, "and my role is", self.role)
 
-# student_1 = Person("John", 37, "DevOps Course", "Student")
+ student_1 = Person("John", 37, "DevOps Course", "Student")
 # student_2 = Person("Mary", 22, "DevOps Course", "Student")
 # coordinator_1 = Person("Jane", 34, "DevOps Course", "Coordinator")
 # instructor_1 = Person("Bruce", 35, "DevOps Course", "Instructor")
